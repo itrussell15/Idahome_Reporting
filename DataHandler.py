962edddc8403fcbb994ca9120ce66e3097f32dc2
Isaac <itrussell15@gmail.com>
2022-06-11 13:35:47 -0600

    Data Wrangling somewhat under control, starting on report generation

--- DataHandler.py
+++ DataHandler.py
@@ -10,14 +10,14 @@ import os
 import matplotlib.pyplot as plt
 
 class DataHandler:
-    
-    def __init__(self):
-        self.df = pd.read_excel(os.getcwd() + "/Data/Data.xlsx")
+
+    def __init__(self, data_path):
+        self.df = pd.read_excel(data_path)
         self.df["Lead Status"].fillna("Disposition Not Set", inplace = True)
         self.df["Lead Source"].fillna("No Lead Source", inplace = True)
         self.df["Lead Owner"].fillna("No Owner", inplace = True)
-        
-        
+        self.df["Office"].fillna("No Office", inplace = True)
+
     def getCloserData(self, name):
         if name in self.df["Lead Owner"].unique():
             closerData = self.df.loc[self.df["Lead Owner"] == name].copy()
@@ -25,52 +25,109 @@ class DataHandler:
             return self._CloserData(name, closerData)
         else:
             raise KeyError("{} has no leads in this data".format(name))
-        
-    class _CloserData:
-        
+
+    def getOfficeData(self, name):
+        if name in self.df["Office"].unique():
+            officeData = self.df.loc[self.df["Office"] == name].copy()
+            officeData.drop("Office", axis = 1, inplace = True)
+            return self._OfficeData(name, officeData)
+        else:
+            raise KeyError('{} has no leads in this data'.format(name))
+            
+    def getAllData(self):
+        return self._AllData(self.df.copy())
+    
+    class _ReportableData:
+
         def __init__(self, name, raw_data):
             self.name = name
             self.leads = raw_data
+            self.numSigns = self._getSigns()
+            self.numLeads = len(self.leads)
+            self.closeRatio = 100*(self.numSigns/self.numLeads)
+    
+        def _groupedOutput(self, column):
+            output = self.leads.groupby(column).count()["ID"]
+            output.rename(self.name, inplace = True)
+            return output
+        
+        def _graphByGrouped(self, column, percentages = False, color = "b", save_image = False):
+            data = self._groupedOutput(column)
+            fig = plt.figure(figsize = (50, 50))
+            ax = plt.subplot(111)
+            plt.title("{}'s {}s".format(self.name, column))
+            graph = ax.barh(data.index, data.values, color = color)
+            ax.spines['right'].set_visible(False)
+            ax.spines['top'].set_visible(False)
+            if percentages:
+                i = 0
+                percentages = 100*(data.values/sum(data.values))
+                for p in graph:
+                    width = p.get_width()
+                    height = p.get_height()
+                    x, y = p.get_xy()
+                    ax.text(x+width+2,
+                              y+height/2,
+                              "{:.2f}%".format(percentages[i]),
+                              ha='center',
+                              weight='bold')
+                    i+=1
+                    
+            if save_image:
+                print("HERE")
+                plt.savefig(os.getcwd() + "/assets/{}_{}.png".format(self.name, column), dpi = 100)
+        
+        def _getSigns(self):
+            return self._groupedOutput("Lead Status")["Signed"]
+        
+        def byGeneration(self):
+            return self._groupedOutput("Lead Source")
+        
+        def graphGeneration(self, percentages = False, color = "orange", save_image = False):
+            self._graphByGrouped("Lead Source", percentages, color, save_image)
+        
+        def byDisposition(self):
+            return self._groupedOutput("Lead Status")
+        
+        def graphDisposition(self, percentages = False, color = "orange", save_image = False):
+            self._graphByGrouped("Lead Status", percentages, color, save_image)
+    
+    class _CloserData(_ReportableData):
+
+        def __init__(self, closer_name, data):
+            super().__init__(closer_name, data)
             
-data = DataHandler()
+        
+    class _OfficeData(_ReportableData):
 
-z = data.getCloserData("Zach Trussell")
+        def __init__(self, officeName, data):
+            super().__init__(officeName, data)
             
-        # df["Lead Status"].fillna("Disposition Not Set", inplace = True)
-        # df["Lead Source"].fillna("No Lead Source", inplace = True)
-        # df["Lead Owner"].fillna("No Owner", inplace = True)
-        # owners = df["Lead Owner"].unique()
-        # byStatus = df.groupby(["Lead Owner", "Lead Status"]).size()
-        # byGen  = df.groupby(["Lead Owner", "Lead Source"]).size()
-
-# disposition = {}
-# generation = {}
-# for i in owners:
-#     disposition.update({i: byStatus[i].to_dict()})
-#     generation.update({i: byGen[i].to_dict()})
+        def byCloser(self):
+            return super()._groupedOutput("Lead Owner")
+        
+        def graphCloser(self, percentages = False, color = "orange", save_image = False):
+            self._graphByGrouped("Lead Owner", percentages, color, save_image)    
+            
+    class _AllData(_ReportableData):
+        
+        def __init__(self, data):
+            super().__init__("All", data)
 
 
-# dispo = pd.DataFrame.from_records(disposition)
-# generation = pd.DataFrame.from_records(generation)
-# dispo.fillna(0, inplace = True)
-# generation.fillna(0, inplace = True)
 
-# fig, axes = plt.subplots(1, 2)
-# fig.suptitle("Zach Trussell")
+if __name__ == "__main__":
+    
+    data = DataHandler(os.getcwd() + "/Data/Data.xlsx")
+    # z = data.getCloserData("Zach Trussell")
+    # z.graphGeneration(True)
+    office = data.getOfficeData("Idahome Electric")
+    office.graphCloser(True, save_image = True)
+    # office.graphDisposition(True, "g")
 
-# def addPercentages(graph, data, axis):
-#     i = 0
-#     percentages = 100*(data.values/sum(data.values))
-#     for p in graph:
-#         width = p.get_width()
-#         height = p.get_height()
-#         x, y = p.get_xy()
-#         axis.text(x+width+4,
-#                  y+height/2,
-#                  "{:.2f}%".format(percentages[i]),
-#                  ha='center',
-#                  weight='bold')
-#         i+=1
+    # z = data.getCloserData("Zach Trussell")
+    # print(z.byDisposition())
+
 
 # pieces = dispo["Zach Trussell"][dispo["Zach Trussell"] > 0]
 # graph = axes[0].barh(pieces.index, pieces.values)
--- ReportGenerator.py
+++ ReportGenerator.py
@@ -6,16 +6,19 @@ Created on Sat Jun 11 00:07:17 2022
 """
 
 from fpdf import FPDF
-import datetime
+from DataHandler import DataHandler
+import datetime, os
 
 class Report(FPDF):
     
-    def __init__(self, title):
+    def __init__(self, type_, title, path):
         super().__init__()
         self.WIDTH = 210
         self.HEIGHT = 297
         self._title = title
         self._font = "Courier"
+        self._type = type_
+        self._data = DataHandler(path)
         
     def header(self):
         self.image("assets/logo.png", 10, 8, 33)
@@ -25,7 +28,7 @@ class Report(FPDF):
         # Move to the right
         self.cell(80)
         # Title
-        self.cell(60, 10, "Closer Report", align = "C")
+        self.cell(60, 10, "{} Report".format(self._type), align = "C")
         self.ln(11)
         self.set_font(self._font, 'B', 15)
         self.cell(10+80+130, 10, self._title, align = "C")
@@ -38,19 +41,29 @@ class Report(FPDF):
         self.set_y(-10)
         self.cell(self.WIDTH,10, "Report Generated at {}".format(thisTime.strftime("%m/%d/%Y, %H:%M:%S")), align = "C")
         
+    def output(self):
+        thisTime = datetime.datetime.now()
+        super().output("output/{}_{}_Report_{}.pdf".format(self._title, self._type, thisTime.strftime("%m-%d-%Y"), "F"))
+        
 class CloserReport(Report):
     
-    def __init__(self, closer_name):
-        super().__init__(closer_name)
+    def __init__(self, closer_name, path):
+        super().__init__("Closer", closer_name, path)
+        
         
 class OfficeReport(Report):
     
-    def __init__(self):
-        super().__init__("Office Report")
+    def __init__(self, office, path):
+        super().__init__("Office", office, path)
         
+class AllReport(Report):
+    
+        def __init__(self, path):
+            super().__init__("All", "", path)
 
 if __name__ == "__main__":
-    report = CloserReport("Vin Santangelo")
-    # report = OfficeReport()
+    path = os.getcwd() + "/Data/Data.xlsx"
+    report = CloserReport("Vin Santangelo", path)
+    # report = OfficeReport("Idahome Electric", path)
     report.add_page()
-    report.output("test1.pdf", "F")
+    report.output()
