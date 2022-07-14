d55539fc27a66dbf7d54d53bac5305b7c6cac60d
Isaac <itrussell15@gmail.com>
2022-07-14 12:43:53 -0600

    Wrapper work.

--- Data/EnerfloWrapper.py
+++ Data/EnerfloWrapper.py
@@ -7,6 +7,7 @@ Created on Wed Jul 13 22:45:21 2022
 
 import requests
 import datetime
+import pandas as pd
 
 class EnerfloWrapper:
     
@@ -18,6 +19,7 @@ class EnerfloWrapper:
                         "all_customers": "v1/customers"
                     }
             }
+        self.requestCount = 0
         
     def _loadToken(self):
         with open("Secret.txt", "r") as f:
@@ -28,36 +30,60 @@ class EnerfloWrapper:
         return self.BASE_URL.format(endpoint)
     
     # Put get request with pages here
-    def _get(self, url, params):
-        r = requests.get(url, params = params, headers = self._headers)
-        # Returns json if good
-        if r.status_code == 200:
+    def _get(self, url, params = None, as_json = True):
+        try:
+            r = requests.get(url, params = params, headers = self._headers)
+            self.requestCount += 1
+            r.raise_for_status()
+            
+        except requests.exceptions.HTTPError as err:
+            raise requests.exceptions.HTTPError(err)
+        except Exception as err:
+            raise err
+        
+        if as_json:
             return r.json()
-        # Raises errors with code if bad.
-        # else:
-            # raise RequestError
+        else:
+            return r
+
     
     # Get customers 
-    def getCustomers(self, pageSize = 500):
+    def getCustomers(self, pageSize = 400):
         
         url = self._getURL(self._endpoints["GET"]["all_customers"])
 
         # Get the number of data points to collect for next time.
-        numLeads = requests.get(url, headers = self._headers).json()["dataCount"]
-        
+        # numLeads = requests.get(url, headers = self._headers).json()["dataCount"]
+        r = self._get(url)
+        numLeads = r["dataCount"]
         # Get the number of pages we need to run through to get all the leads
         numPages = numLeads//pageSize
         # If there is the division isn't spot on, then add an extra page
         if numLeads % pageSize != 0:
             numPages += 1
         
+        # print(numPages)
         # TODO Start at the end of all the pages and move backwards rather than forward until we get dates that are within our date range that we want.
         for i in range(numPages, 0, -1):
             print("Requesting Page -{}-".format(i))
             params = {"pageSize": pageSize, "page": i}
-            page = self._get(url, params)
-
-
+            page = self._get(url, params, as_json = False)
+            remainingReqs = page.headers["X-RateLimit-Remaining"]
+            print("Received Page -{}-, {} Remaining Requests".format(i, remainingReqs))
+            # Info needed Name, Lead Source, Lead Status, Setter, Lead Owner, Next Appointment, Added
+            pageLeads = self.extractLeadData(page.json()["data"])
+            
+    def extractLeadData(self, pageData):
+        # df = pd.DataFrame()
+        data = []
+        for lead in pageData:
+            leadData = {
+                "name": f"""{lead["first_name"]} {lead["last_name"]}""",
+                "email": lead["email"],
+                "phone": lead["mobile"],
+                "address": f"""{lead["address"]} {lead["city"]}, {lead["state"]} {lead["zip"]}"""
+                        }
+            print(leadData)
 if __name__ == "__main__":
     wrapper = EnerfloWrapper()
     
